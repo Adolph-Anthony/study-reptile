de5fe618a5f117c68e5a1927ae4b74ae68c96534
Athony·xu <1137847433@qq.com>
2019-07-21 21:47:52 +0800

    jar转化

---
+++ requests_test/7.utils.py
@@ -0,0 +1,18 @@
+import requests
+
+# 目标url
+url = 'https://www.baidu.com'
+
+# 发起请求
+response = requests.get(url)
+
+print(response.cookies)
+
+
+dict_cookies = requests.utils.dict_from_cookiejar(response.cookies)
+print(dict_cookies)
+
+
+# 因为后续的没能得到域名
+jar_cookies = requests.utils.cookiejar_from_dict(dict_cookies)
+print(jar_cookies)
